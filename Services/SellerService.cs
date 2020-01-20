e08d952fe5b16917686b1176645dc98c01144499
Filipe Albieri <filipe.albieri@resourceit.com>
2020-01-20 14:11:22 -0300

    Simple Create form

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -20,5 +20,11 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller objeto)
+        {
+            _context.Add(objeto);
+            _context.SaveChanges();
+        }
     }
 }
